6d18cf1437c42774bed1f347c13a741344c57fa0
TarinaSnyman <tarina.snyman@gmail.com>
2025-05-13 14:57:12 +0200

    Added images Changed team members Contact submission Added a paragraph to home page

--- community-portal-prefilled/routes/pageRoutes.js
+++ community-portal-prefilled/routes/pageRoutes.js
@@ -2,10 +2,9 @@
 
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
 
 router.get('/', (req, res) => {
-
-   
     res.render('pages/home')
 });
 
@@ -18,7 +17,7 @@ router.get('/about', (req, res) => {
 
 router.get('/events', (req, res) => {
     const data = { events: req.events };
-      res.send("hello events")
+    res.render('pages/events', data);
 });
 
 router.get('/contact', (req, res) => {
@@ -31,12 +30,31 @@ router.get('/contact', (req, res) => {
       res.render('pages/contact');
 
 });
+// POST /contact
+router.post('/contact', (req, res) => {
+    const { name, email, message } = req.body;
+    if (name && email && message) {
+        const submission = { name, email, message };
+        req.contactSubmissions.push(submission);
+
+        // write to aJSON file
+        fs.writeFile('submissions.json', JSON.stringify(req.contactSubmissions, null, 2), (err) => {
+            if (err) {
+                console.error('Error writing to JSON file:', err);
+            }
+        });
+
+        res.redirect('/thankyou');
+    } else {
+        res.render('pages/contact', { error: 'All fields are required' });
+    }
+});
 
 router.get('/thankyou', (req, res) => {
     const lastSubmission = req.contactSubmissions[req.contactSubmissions.length - 1];
     const data = { name: lastSubmission?.name || 'Guest' };
 
-    res.render('pages/thankyou');
+    res.render('pages/thankyou',data);
 
 });
 
